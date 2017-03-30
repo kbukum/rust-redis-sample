09533b4bba14a3b686b3b6c5d60624a9911d9255
serayuzgur <serayuzgur@gmail.com>
2017-03-30 11:22:15 +0300

    get op. fixed.

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -14,18 +14,19 @@ pub fn put_struct<T: Serialize>(key: &str, st: T) -> redis::RedisResult<T> {
     let conn = try!(client.get_connection());
     // Encode our Job in JSON
     let serialized = serde_json::to_string(&st).unwrap();
-     println!("\tSerialized : {}", &serialized);
-    let _ : () = try!(conn.set(key, &serialized));
+    println!("\tSerialized : {}", &serialized);
+    let _: () = try!(conn.set(key, &serialized));
     // println!("Struct: {:?}", st);
     Ok(st)
 }
 
-/*
-pub fn get_struct<T: Deserialize, E>(key: &str) -> Result<T, E> {
-   // Connect to a local Redis
+
+pub fn get_struct<T: Deserialize>(key: &str) -> redis::RedisResult<T> {
+    // Connect to a local Redis
     let client = try!(redis::Client::open("redis://127.0.0.1/"));
+
     let conn = try!(client.get_connection());
-    let serialized_struct : String = try!(conn.get(key));
+    let serialized_struct: String = try!(conn.get(key));
     Ok(serde_json::from_str(&serialized_struct).unwrap())
 }
-*/
+
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate serde;
 extern crate serde_json;
 extern crate redis;
+
 use std::string::String;
 mod cache;
 
@@ -22,7 +23,16 @@ fn main() {
     // Create a Struct
     let example: ExampleStruct = ExampleStruct { name: "Kamil Bukum".to_string()};
     let name = "example".to_string();
-    cache::put_struct(name.as_ref(), example);
+    let put_result = cache::put_struct(name.as_ref(), &example).unwrap();
+    //Print the operation result.
+    println!("{:?}",&put_result);
+
+    // Get the result from redis.
+    let get_result: ExampleStruct = cache::get_struct(name.as_ref()).unwrap();
+    // Print the result.
+    println!("{:?}",&get_result);
+    assert_eq!(example.name, put_result.name, "Names must be same Original: {}, Redis(Put): {}", example.name, put_result.name);
+    assert_eq!(example.name, get_result.name, "Names must be same Original: {}, Redis(Get): {}", example.name, get_result.name);
 }
 
 
